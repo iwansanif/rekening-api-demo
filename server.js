27bbe1858d865babd267dee7e14294afb3aa2540
Iwan Sanif <iwanbsanif@gmail.com>
2026-07-11 15:35:18 +0700

    Release v1.0.0 client demo version

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const app = require('./src/app');
 const env = require('./src/config/env');
 
+
 app.listen(env.port, () => {
 
     console.log('====================================');
--- src/app.js
+++ src/app.js
@@ -3,46 +3,87 @@ const helmet = require('helmet');
 const cors = require('cors');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const path = require('path');
 
 const routes = require('./routes');
 
 const apiKeyMiddleware = require('./middlewares/api-key.middleware');
 const errorHandler = require('./middlewares/error-handler.middleware');
 
-const app = express();
-
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./config/swagger');
 
-app.use(helmet());
+const app = express();
+
+
+// Security
+app.use(
+    helmet({
+        contentSecurityPolicy: false
+    })
+);
+
+
+// CORS
 app.use(cors());
+
+
+// JSON Parser
 app.use(express.json());
 
+
+// Frontend Demo
+app.use(
+    express.static(
+        path.join(__dirname, '../public')
+    )
+);
+
+
+// Swagger Documentation
 app.use(
     '/api-docs',
     swaggerUi.serve,
     swaggerUi.setup(swaggerSpec)
 );
 
+
+// Logging
 app.use(morgan('dev'));
 
+app.use(
+    require('./middlewares/access-log.middleware')
+);
+
+
+// Rate Limit
 const limiter = rateLimit({
+
     windowMs: 24 * 60 * 60 * 1000,
+
     max: 3000,
+
     standardHeaders: true,
+
     legacyHeaders: false
+
 });
 
-app.use(morgan('dev'));
 
-app.use(require('./middlewares/access-log.middleware'));
+// API protection hanya untuk API
+app.use(
+    '/api',
+    limiter,
+    apiKeyMiddleware
+);
 
-app.use(limiter);
 
-app.use(apiKeyMiddleware);
-
+// Routes
 app.use(routes);
 
+
+// Error Handler
 app.use(errorHandler);
+
 
 module.exports = app;
